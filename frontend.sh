e9ab15f8695e804114b09bf9be85aa4ce7c11e0b
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-29 21:11:30 +0530

    frontend setup shell

---
+++ frontend.sh
@@ -0,0 +1,17 @@
+set_loaction=$(pwd)
+yum install nginx
+
+systemctl enable nginx
+
+systemctl start nginx
+
+rm -rf  /usr/share/nginx/html/*
+
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+cp ${set_loaction}/files/roboshop.conf  /etc/nginx/default.d
