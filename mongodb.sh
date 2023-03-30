43a95989e7edc37b5d3f5845c24754dc278895ac
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-30 11:38:12 +0530

    mongodb

---
+++ mongodb.sh
@@ -0,0 +1,12 @@
+set_location=$(pwd)
+
+cp ${set_location}/files/mongodb.repo  /etc/yum.repo.d/mongo.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf
+
+systemctl restart mongod
