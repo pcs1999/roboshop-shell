3e53cef26323a6872578b8e9a20fac05dfc2ebac
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-30 21:55:36 +0530

    updating ./roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,39 +1,82 @@
 set_location=$(pwd)
 LOG=/tmp/roboshop.log
+source common.sh
 
-
+echo -e "\e[32m Setup NodeJS repos \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+condition_check
 
+echo -e "\e[32m installing nodejs \e[0m"
 yum install nodejs -y &>>${LOG}
+condition_check
 
+echo -e "\e[32m adding user roboshop \e[0m"
 useradd roboshop &>>${LOG}
+condition_check
 
 mkdir -p /app &>>${LOG}
 
+
+echo -e "\e[32m downloading content \e[0m"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
+condition_check
 
+echo -e "\e[32m   removing any old content\e[0m"
 rm -rf /app/* &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m change to app directory \e[0m"
 cd /app &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m unziping the content of catalogue  \e[0m"
 unzip /tmp/catalogue.zip &>>${LOG}
+condition_check
 
+echo -e "\e[32m  change to app directory \e[0m"
 cd /app &>>${LOG}
+condition_check
 
+echo -e "\e[32m node packages inatlling \e[0m"
 npm install &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m copying catalogue service file \e[0m"
 cp ${set_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m  reload the system setup \e[0m"
 systemctl daemon-reload &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m enable catalogue \e[0m"
 systemctl enable catalogue &>>${LOG}
+condition_check
+
+
+echo -e "\e[32m strting catalogue \e[0m"
 systemctl start catalogue &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m copying repo file \e[0m"
 cp ${set_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m installing mongod shell from repo \e[0m"
 yum install mongodb-org-shell -y &>>${LOG}
+condition_check
 
+
+echo -e "\e[32m redirecting js files \e[0m"
 mongo --host mongodb-dev.chandupcs.online </app/schema/catalogue.js &>>${LOG}
+condition_check
 
 
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,27 @@
 set_location=$(pwd)
+LOG=/tmp/roboshop.log
+source common.sh
 
-cp ${set_location}/files/mongodb.repo  /etc/yum.repos.d/mongo.repo
+echo -e "\e[32m  downloading mongo.repo file ro\e[0m"
+cp ${set_location}/files/mongodb.repo  /etc/yum.repos.d/mongo.repo &>>${LOG}
+condition_check
 
-yum install mongodb-org -y
+echo -e "\e[32m installing mongodb \e[0m"
+yum install mongodb-org -y &>>${LOG}
+condition_check
 
-systemctl enable mongod
-systemctl start mongod
+echo -e "\e[32m enabling mongodb \e[0m"
+systemctl enable mongod &>>${LOG}
+condition_check
 
-sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf
+echo -e "\e[32m  start mongodb \e[0m"
+systemctl start mongod &>>${LOG}
+condition_check
 
-systemctl restart mongod
+echo -e "\e[32m changing port to 0.0.0.0  \e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf &>>${LOG}
+condition_check
+
+echo -e "\e[32m  \e[0m"
+systemctl restart mongod &>>${LOG}
+condition_check
