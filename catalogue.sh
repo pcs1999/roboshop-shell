cde6c3c3f636cd5abdd1baacef3e030abe868fde
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-30 17:13:22 +0530

    catalogue1

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,35 @@
+set_location=$(pwd)
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
 yum install nodejs -y
+
 useradd roboshop
-mkdir /app
+
+mkdir -p /app
+
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
 cd /app
+
+rm -rf *
+
 unzip /tmp/catalogue.zip
+
 cd /app
+
 npm install
+
+cp ${set_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+systemctl start catalogue
+
+cp ${set_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host mongodb-dev.chandupcs.online </app/schema/catalogue.js
+
