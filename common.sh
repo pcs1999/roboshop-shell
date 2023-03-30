1565a82f583e0a0310d7699173dcf048af5badca
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-30 22:23:42 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
 condition_check () {
   if [ $? -eq 0 ]
   then
-    echo echo -e "\e[32m SUCCESS \e[0m"
+    echo  -e "\e[32m SUCCESS \e[0m"
   else
-    echo echo -e "\e[31m FAILURE \e[0m"
+    echo  -e "\e[31m FAILURE \e[0m"
   exit
   echo please refer the log-> ${LOG}
   fi
