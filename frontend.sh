7b5d1626a0f5595e0331fda220ba9d86476a080c
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-30 20:38:34 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,34 @@
 set_loaction=$(pwd)
+LOG=/tmp/roboshop.log
+
 echo -e "\e[32m installing nginx webserver \e[0m"
-yum install nginx -y
+yum install nginx -y &>>{LOG}
+echo $?
 
 echo -e "\e[32m enabling nginx \e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>{LOG}
+echo $?
 
 echo -e "\e[32m start nginx webserver \e[0m"
-systemctl start nginx
-
+systemctl start nginx &>>{LOG}
+echo $?
 
 echo -e "\e[32m remove old content nginx webserver \e[0m"
-rm -rf  /usr/share/nginx/html/*
+rm -rf  /usr/share/nginx/html/*  &>>{LOG}
+echo $?
 
 echo -e "\e[32m downloading the content nginx webserver \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>{LOG}
+echo $?
 
 echo -e "\e[32m change directory nginx webserver \e[0m"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>{LOG}
+echo $?
 
 echo -e "\e[32m uzipping the frontend.zip  \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>{LOG}
+echo $?
 
 echo -e "\e[32m config for proxy nginx webserver \e[0m"
-cp ${set_loaction}/files/nginx-roboshop.conf  /etc/nginx/default.d/roboshop.conf
+cp ${set_loaction}/files/nginx-roboshop.conf  /etc/nginx/default.d/roboshop.conf &>>{LOG}
+echo $?
