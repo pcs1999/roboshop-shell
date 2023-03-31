6c9986b46c12d0e4d09955b828dab01e9907a060
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-31 15:16:35 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -22,3 +22,89 @@ user_check () {
   fi
 
 }
+
+NODEJS (){
+
+source common.sh
+
+print_head " Setup NodeJS repos "
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+condition_check
+
+print_head " installing nodejs "
+yum install nodejs -y &>>${LOG}
+condition_check
+
+print_head " adding ${component} roboshop "
+id roboshop
+${component}_check
+condition_check
+
+mkdir -p /app &>>${LOG}
+
+
+print_head " downloading content "
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+condition_check
+
+print_head "   removing any old content"
+rm -rf /app/* &>>${LOG}
+condition_check
+
+
+print_head " change to app directory "
+cd /app &>>${LOG}
+condition_check
+
+
+print_head " unziping the content of ${component}  "
+unzip /tmp/${component}.zip &>>${LOG}
+condition_check
+
+print_head "  change to app directory "
+cd /app &>>${LOG}
+condition_check
+
+print_head " node packages inatlling "
+npm install &>>${LOG}
+condition_check
+
+
+print_head " copying ${component} service file "
+cp ${set_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+condition_check
+
+
+print_head "  reload the system setup "
+systemctl daemon-reload &>>${LOG}
+condition_check
+
+
+print_head " enable ${component} "
+systemctl enable ${component} &>>${LOG}
+condition_check
+
+
+print_head " strting ${component} "
+systemctl start ${component} &>>${LOG}
+condition_check
+
+
+print_head " copying repo file "
+cp ${set_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+condition_check
+
+
+print_head " installing mongod shell from repo "
+yum install mongodb-org-shell -y &>>${LOG}
+condition_check
+
+
+print_head " redirecting js files "
+mongo --host mongodb-dev.chandupcs.online </app/schema/${component}.js &>>${LOG}
+condition_check
+
+
+
+
+}
