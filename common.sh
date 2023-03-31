07d621158afc2ac946c18c0e51473317ac0d8a31
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-31 15:42:26 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ condition_check () {
     echo  -e "\e[32m SUCCESS \e[0m"
   else
     echo  -e "\e[31m FAILURE \e[0m"
-    echo "please refer the log-> ${LOG}"
+    echo "please refer the log-> $>>{LOG}"
   exit
   fi
 }
