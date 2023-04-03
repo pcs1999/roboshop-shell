fb1ae7881ad97b1eb72f5cc4a49e6885d5b7eed2
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-03 10:08:57 +0530

    updating ./roboshop-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,7 +6,7 @@ if [ -z "${roboshop_mysql_password}" ]; then
 fi
 
 print_head " Configure YUM Repos from the script provided by vendor "
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
 condition_check
 
 print_head " install erlang "
@@ -14,30 +14,33 @@ yum install erlang -y
 condition_check
 
 print_head "downloading packges of raabittmq  "
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
 condition_check
 
 
 print_head "  install rabbitmq-server "
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y &>>${LOG}
 condition_check
 
 print_head "enable rabbitmq-server  "
-systemctl enable rabbitmq-server
+systemctl enable rabbitmq-server &>>${LOG}
 condition_check
 
 print_head "start rabbitmq-server  "
-systemctl start rabbitmq-server
+systemctl start rabbitmq-server &>>${LOG}
 condition_check
 
 print_head " add_user roboshop  "
-rabbitmqctl add_user roboshop ${roboshop_mysql_password}
+rabbitmqctl list users | grep roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop ${roboshop_mysql_password} &>>${LOG}
+fi
 condition_check
 
 print_head "  administrator "
-rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG}
 condition_check
 
 print_head "set_permissions"
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
 condition_check
